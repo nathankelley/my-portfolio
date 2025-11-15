9f7dca14f5ca5e5d765ce94be01f00bcd5b1927c
Nathan Kelley <kelleyjnathan@gmail.com>
2025-11-15 14:15:50 -0700

    Last thing for x icon?

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ export default function Footer() {
                     </a>
                     <a href="https://x.com/N_JKelley" target="_blank" rel="noopener noreferrer"
                         style={socialStyle}>
-                        <i class="fab fa-x-twitter fa-2x"></i>
+                        <i class="fab fa-x-twitter"></i>
                     </a>
                     <a href="https://github.com/nathankelley" target="_blank" rel="noopener noreferrer"
                         style={socialStyle}>
